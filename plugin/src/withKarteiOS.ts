88ba67ab54b50cf95d6e4c980803281d5ac5471c
nkmrh <nkmrhj@gmail.com>
2023-04-30 14:21:14 +0900

    Use Expo Modules API

--- plugin/src/withKarteiOS.ts
+++ plugin/src/withKarteiOS.ts
@@ -1,24 +1,6 @@
 import { ConfigPlugin, withAppDelegate } from "expo/config-plugins";
 import { ConfigProps } from "./types";
 
-const { mergeContents } = require('@expo/config-plugins/build/utils/generateCode');
-
-function addKarteAppDelegateImport(src: any) {
-    const newSrc = ['@import KarteCore;']
-    return mergeContents({
-        tag: 'karte-import',
-        src,
-        newSrc: newSrc.join('\n'),
-        anchor: /#import "AppDelegate\.h"/,
-        offset: 1,
-        comment: '//',
-    })
-}
-
 export const withKarteiOS: ConfigPlugin<ConfigProps> = (config, props) => {
-    // config = withAppDelegate(config, (config) => {
-    //     config.modResults.contents = addKarteAppDelegateImport(config.modResults.contents).contents;
-    //     return config;
-    // });
     return config;
 };
