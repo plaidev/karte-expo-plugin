08e8dfa2ca6f62fddc3a91315c82bfd936ada34e
nkmrh <nkmrhj@gmail.com>
2023-05-11 08:03:04 +0900

    Rename

--- plugin/src/withKarte.ts
+++ plugin/src/withKarte.ts
@@ -1,12 +1,9 @@
 import { ConfigPlugin, createRunOncePlugin } from "expo/config-plugins";
-
 import { ConfigProps } from "./types";
 import { withKarteAndroid } from "./withKarteAndroid";
-import { withKarteInfoPlist } from "./withKarteInfoPlist";
 import { withKarteiOS } from "./withKarteiOS";
 
 const withKarte: ConfigPlugin<ConfigProps> = (config, props) => {
-  config = withKarteInfoPlist(config, props);
   config = withKarteiOS(config, props);
   config = withKarteAndroid(config, props);
   return config;
--- plugin/src/withKarteAndroid.ts
+++ plugin/src/withKarteAndroid.ts
@@ -9,20 +9,6 @@ import { ConfigProps } from "./types";
 import path from 'path';
 import fs from 'fs';
 
-function copyKarteXml(
-  config: ExportedConfigWithProps,
-  props: ConfigProps
-) {
-  if (!props.karteXml) {
-    throw new Error(
-      'Path to karte.xml is not defined. Please specify the `expo.android.karteXml` field in app.json.',
-    );
-  }
-  const srcPath = path.resolve(config.modRequest.projectRoot , props.karteXml);
-  const destPath = path.resolve(config.modRequest.platformProjectRoot, 'app/src/main/res/values/karte.xml');
-  fs.copyFileSync(srcPath, destPath);
-}
-
 export const withKarteAndroid: ConfigPlugin<ConfigProps> = (config, props) => {
   config = withDangerousMod(config, [
     'android',
@@ -40,4 +26,19 @@ export const withKarteAndroid: ConfigPlugin<ConfigProps> = (config, props) => {
     return config;
   });
   return config;
+};
+
+
+function copyKarteXml(
+  config: ExportedConfigWithProps,
+  props: ConfigProps
+) {
+  if (!props.karteXml) {
+    throw new Error(
+      'Path to karte.xml is not defined. Please specify the `expo.android.karteXml` field in app.json.',
+    );
+  }
+  const srcPath = path.resolve(config.modRequest.projectRoot , props.karteXml);
+  const destPath = path.resolve(config.modRequest.platformProjectRoot, 'app/src/main/res/values/karte.xml');
+  fs.copyFileSync(srcPath, destPath);
 };
